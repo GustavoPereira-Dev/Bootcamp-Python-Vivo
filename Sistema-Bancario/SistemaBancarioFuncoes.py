3d51571a47fc8cc26c61adfa25c2266d1635848c
GustavoPereira-Dev <gustavopereira8919@gmail.com>
2024-07-05 21:45:10 -0300

    Desenvolvendo o projeto 'Imagem do bootcamp Modelando o Sistema Bancário em POO com Python'

--- Sistema-Bancario/SistemaBancarioFuncoes.py
+++ Sistema-Bancario/SistemaBancarioFuncoes.py
@@ -1,10 +1,15 @@
+import re
+
 def menu():
     menu = """
     ==================== SISTEMA BANCÁRIO ====================
-    |                    *[d] Depositar                      |
-    |                    *[s] Sacar                          |
-    |                    *[e] Extrato                        |
-    |                    *[q] Sair                           |
+    |                    *[d]   Depositar                    |
+    |                    *[s]   Sacar                        |
+    |                    *[e]   Extrato                      |
+    |                    *[nvc] Nova conta                   |
+    |                    *[lc]  Listar contas                |
+    |                    *[nvu] Novo usuário                 |
+    |                    *[q]   Sair                         |
     ==========================================================
     => """
 
@@ -42,13 +47,66 @@ def emitirExtrato(saldo,/,*,extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("==========================================")
 
+def filtrarUsuario(cpf, usuarios):
+    filtragem = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return filtragem[0] if filtragem else None
+
+def criarConta(agencia, nConta, usuarios):
+    cpf = input("Por favor, informe o CPF do usuário: ")
+    usuario = filtrarUsuario(cpf, usuarios)
+
+    if usuario:
+        print(f"Conta do usuário do número de agência {agencia} número da conta {nConta} e cpf {cpf} criado com sucesso")
+        return{"agencia": agencia, "numeroConta": nConta, "usuario": usuario}
+    print("Infelizmente o usuário de determinado CPF não foi encontrado")
+
+def criarUsuario(usuarios):
+    cpf = input("Por favor, informe o CPF do usuário: ")
+    usuario = filtrarUsuario(cpf, usuarios)
+    
+    if usuario:
+        print("Já existe um usuário com esse CPF!")
+        return
+    elif not re.match(r"\d{11}", cpf):
+        print("Essa numeração não bate com um CPF comum!")
+        return
+
+    nome = input("Informe o nome: ")
+    dataNascimento = input("Informe a data de nascimento do usuário no formato (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço do usuário no formato (logradouro, nro - bairro - cidade/sigla estado): ")
+    usuarios.append({"nome": nome, "dataNascimento": dataNascimento, "cpf": cpf, "endereco": endereco})
+
+    print("Usuario criado com sucesso!")
+
+def listarContas(contas):
+    if len(contas) > 0:
+        for conta in contas:
+            lista = f"""
+                Agência:              {conta['agencia']}
+                Número da conta:      {conta['numeroConta']}
+                Usuário:              {conta['usuario']['nome']}
+            """
+            print("=" * 60)
+            print(lista)
+    else:
+        print("Não existem contas para visualizar!")
+        
+
+
+    
+
 
 def main():
     saldo = 0
     limiteSaldo = 500
     extrato = ""
     numeroSaques = 0
+
+    lUsuarios = []
+    lContas = []
+
     LIMITE_SAQUES = 3
+    AGENCIA = "0001"
     while True:
 
         opcao = menu()
@@ -74,6 +132,20 @@ def main():
                     saldo,
                     extrato=extrato
                 )
+                
+            case "nvc" | "NVC":
+                numeroConta = len(lContas) + 1
+                conta = criarConta(AGENCIA, numeroConta, lUsuarios)
+
+                if conta:
+                    lContas.append(conta)
+                    
+            case "lc" | "LC":
+                listarContas(lContas)
+
+            case "nvu" | "NVU":
+                criarUsuario(lUsuarios)
+                
             case "q" | "Q":
                 print("Saindo do sistema...")
                 break
